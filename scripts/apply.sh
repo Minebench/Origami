c453017e21cdef8cc8f033f4889c3260220e369b
Phoenix616 <mail@moep.tv>
2020-02-05 16:44:42 +0100

    Add argument to define whether the api or the server should be patched

--- scripts/apply.sh
+++ scripts/apply.sh
@@ -83,12 +83,36 @@ function enableCommitSigningIfNeeded {
     fi
 }
 
-(
-    (applyPatch Paper/Paper-API ${FORK_NAME}-API HEAD api $API_REPO &&
-    applyPatch Paper/Paper-Server ${FORK_NAME}-Server HEAD server $SERVER_REPO) || exit 1
-    enableCommitSigningIfNeeded
-) || (
-    echo "Failed to apply patches"
-    enableCommitSigningIfNeeded
-    exit 1
-) || exit 1
+case "$1" in
+    "a" | "api")
+    (
+        applyPatch Paper/Paper-API ${FORK_NAME}-API HEAD api $API_REPO || exit 1
+        enableCommitSigningIfNeeded
+    ) || (
+        echo "Failed to apply api patches"
+        enableCommitSigningIfNeeded
+        exit 1
+    ) || exit 1
+    ;;
+    "s" | "server")
+    (
+        applyPatch Paper/Paper-Server ${FORK_NAME}-Server HEAD server $SERVER_REPO || exit 1
+        enableCommitSigningIfNeeded
+    ) || (
+        echo "Failed to apply server patches"
+        enableCommitSigningIfNeeded
+        exit 1
+    ) || exit 1
+    ;;
+    *)
+    (
+        (applyPatch Paper/Paper-API ${FORK_NAME}-API HEAD api $API_REPO &&
+        applyPatch Paper/Paper-Server ${FORK_NAME}-Server HEAD server $SERVER_REPO) || exit 1
+        enableCommitSigningIfNeeded
+    ) || (
+        echo "Failed to apply patches"
+        enableCommitSigningIfNeeded
+        exit 1
+    ) || exit 1
+    ;;
+esac
